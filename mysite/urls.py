f06bf29f11b7afca2bf6526d9d1759a60661c13d
Pranav Gupta <gpranav77@gmail.com>
2017-05-11 19:05:20 +0530

    urls

--- mysite/urls.py
+++ mysite/urls.py
@@ -20,5 +20,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-	url(r'^$', include('phoenix.urls')),
+	url(r'^phoenix/', include('phoenix.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
