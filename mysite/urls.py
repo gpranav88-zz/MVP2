5d249ff6de2886da58f19896b0ea4923383eed7c
Prateek Sachan <ps@prateeksachan.com>
2017-05-11 13:15:39 +0530

    changes

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,6 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^phoenix/', include('phoenix.urls')),
     url(r'^admin/', admin.site.urls),
+	url(r'^$', include('phoenix.urls')),
 ]
--- phoenix/models.py
+++ phoenix/models.py
@@ -2,13 +2,24 @@ from django.db import models
 
 
 class Signal(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, default="None")
+    def __str__(self):
+        return self.name
 
 class Rule(models.Model):
+    name = models.CharField(max_length=50, default="None")
     services = models.TextField(max_length=200)
     attributes = models.TextField(max_length=300)
 
+    def __str__(self):
+        return self.name
+
 class Trigger(models.Model):
+    name = models.CharField(max_length=50, default="None")
     signal_id = models.ForeignKey(Signal, on_delete=models.CASCADE)
     rules = models.CharField(max_length=50)
     threshold_level = models.CharField(max_length=50)
+    action_taken = models.CharField(max_length=50, default="None")
+
+    def __str__(self):
+        return self.name
