47643776a2296bbab3f1b8c1b18a37b45de85e52
Prateek Sachan <ps@prateeksachan.com>
2017-05-11 15:32:23 +0530

    updated shcema

--- phoenix/models.py
+++ phoenix/models.py
@@ -13,17 +13,34 @@ class Signal(models.Model):
         return self.name
 
 class Rule(models.Model):
+    SERVICES_CHOICES = (
+        ('identity','IDENTITY'),
+        ('order', 'ORDER'),
+    )
+    ATTRIBUTES_CHOICES = (
+        ('email','User Email'),
+        ('cancelled_order', 'Cancelled Orders'),
+        ('refund_order', 'Refund Orders'),
+    )
     name = models.CharField(max_length=50, default="None")
-    services = models.TextField(max_length=200)
-    attributes = models.TextField(max_length=300)
+    services = models.CharField(max_length=10, default='identity', choices=SERVICES_CHOICES)
+    attributes = models.CharField(max_length=10, default='email', choices=ATTRIBUTES_CHOICES)
 
     def __str__(self):
         return self.name
 
 class Trigger(models.Model):
+    OPERATION_CHOICES = (
+        ('lt','Less Than'),
+        ('lte', 'Less Than Equal To'),
+        ('eq', 'Equal To'),
+        ('gte', 'Greater Than'),
+        ('gt', 'Greater Than Equal To'),
+    )
     name = models.CharField(max_length=50, default="None")
     signal_id = models.ForeignKey(Signal, on_delete=models.CASCADE)
     rules = models.ManyToManyField(Rule)
+    operation = models.CharField(max_length=10, default='lt', choices=OPERATION_CHOICES)
     threshold_level = models.CharField(max_length=50)
     action_taken = models.ForeignKey(Action, on_delete=models.CASCADE)
 
