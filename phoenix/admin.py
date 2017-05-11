f360f8dd73bb6d0ddb9a0ce563352a438a0122d5
Prateek Sachan <ps@prateeksachan.com>
2017-05-11 13:41:34 +0530

    schema updates

--- phoenix/admin.py
+++ phoenix/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Signal, Rule, Trigger
+from .models import Signal, Rule, Trigger, Action
 
 admin.site.register(Signal)
 admin.site.register(Rule)
 admin.site.register(Trigger)
+admin.site.register(Action)
--- phoenix/models.py
+++ phoenix/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 
 
+class Action(models.Model):
+    name = models.CharField(max_length=50, default="None")
+    key = models.CharField(max_length=50, default="None")
+    def __str__(self):
+        return self.name
+
 class Signal(models.Model):
     name = models.CharField(max_length=50, default="None")
     def __str__(self):
@@ -17,9 +23,9 @@ class Rule(models.Model):
 class Trigger(models.Model):
     name = models.CharField(max_length=50, default="None")
     signal_id = models.ForeignKey(Signal, on_delete=models.CASCADE)
-    rules = models.CharField(max_length=50)
+    rules = models.ManyToManyField(Rule)
     threshold_level = models.CharField(max_length=50)
-    action_taken = models.CharField(max_length=50, default="None")
+    action_taken = models.ForeignKey(Action, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
