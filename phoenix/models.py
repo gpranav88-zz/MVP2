9d6624f273b4abee383192792d01fb938f443397
Prateek Sachan <ps@prateeksachan.com>
2017-05-12 14:11:31 +0530

    saving

--- phoenix/models.py
+++ phoenix/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from multiselectfield import MultiSelectField
 
-
 class Action(models.Model):
     name = models.CharField(max_length=50, default="None")
     key = models.CharField(max_length=50, default="None")
@@ -17,6 +16,8 @@ class Rule(models.Model):
         ('email','User Email'),
         ('cancelled_order', 'Cancelled Orders'),
         ('refund_order', 'Refund Orders'),
+        ('order_count', 'Total Orders'),
+        ('margin', 'Margin'),
     )
     name = models.CharField(max_length=50, default="None")
     services = models.CharField(max_length=10, default='identity', choices=SERVICES_CHOICES)
@@ -37,7 +38,8 @@ class Trigger(models.Model):
     rules = models.ManyToManyField(Rule)
     operation = models.CharField(max_length=10, default='lt', choices=OPERATION_CHOICES)
     threshold_level = models.CharField(max_length=50)
-    action_taken = models.ForeignKey(Action, on_delete=models.CASCADE)
+    action_taken = models.ForeignKey(Action, on_delete=models.CASCADE, related_name='triggers')
+    status = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
